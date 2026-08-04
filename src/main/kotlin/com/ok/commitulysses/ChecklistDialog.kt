4bcde1f51cef6077c84e28d3353304d4e7c08953
dhrgodms <dhrgodms123@gmail.com>
2026-08-04 12:36:55 +0900

    feat: add check all

--- src/main/kotlin/com/ok/commitulysses/ChecklistDialog.kt
+++ src/main/kotlin/com/ok/commitulysses/ChecklistDialog.kt
@@ -2,7 +2,9 @@ package com.ok.commitulysses
 
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.DialogWrapper
+import com.intellij.ui.components.ActionLink
 import com.intellij.ui.components.JBCheckBox
+import com.intellij.ui.dsl.builder.AlignX
 import com.intellij.ui.dsl.builder.panel
 import java.awt.Dimension
 import javax.swing.JComponent
@@ -13,6 +15,7 @@ class ChecklistDialog(
 ) : DialogWrapper(project) {
 
     private val itemCheckboxPairs = mutableListOf<Pair<ChecklistItem, JBCheckBox>>()
+    private var selectAllLink: ActionLink? = null
 
     init {
         title = "Commit Ulysses"
@@ -29,6 +32,8 @@ class ChecklistDialog(
             row {
                 label("Please confirm the following before committing")
                     .applyToComponent { font = font.deriveFont(font.style or java.awt.Font.BOLD, 14f) }
+                    .resizableColumn()
+                selectAllLink = link("Select All") {}.align(AlignX.RIGHT).component
             }
             row {
                 comment("The commit will be blocked until all items are checked.")
@@ -40,6 +45,7 @@ class ChecklistDialog(
                         row {
                             icon(ChecklistIcon.fromName(item.iconName).icon)
                             val cb = checkBox(item.text).component
+                            cb.addItemListener { updateSelectAllLinkText() }
                             itemCheckboxPairs.add(item to cb)
                         }
                     }
@@ -47,8 +53,19 @@ class ChecklistDialog(
             }
         }
 
+        selectAllLink?.addActionListener {
+            val checkAll = itemCheckboxPairs.any { !it.second.isSelected }
+            itemCheckboxPairs.forEach { it.second.isSelected = checkAll }
+        }
+        updateSelectAllLinkText()
+
         dialogPanel.preferredSize = Dimension(480, dialogPanel.preferredSize.height)
         return dialogPanel
+    }
+
+    private fun updateSelectAllLinkText() {
+        val allChecked = itemCheckboxPairs.isNotEmpty() && itemCheckboxPairs.all { it.second.isSelected }
+        selectAllLink?.text = if (allChecked) "Deselect All" else "Select All"
     }
 
     override fun getPreferredFocusedComponent(): JComponent? = itemCheckboxPairs.firstOrNull()?.second
